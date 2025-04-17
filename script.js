2d2ad60970f4d7f06399be7a5814492a23eb4e87
Jyay <anjiolusanya@gmail.com>
2025-04-17 12:54:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,22 +12,3 @@ const navbar = document.getElementById("navbar");
   menuToggle.addEventListener("click", () => {
     navbar.classList.toggle("show");
 });
-
-// Dropdown toggles
-const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
-
-  dropdownToggles.forEach(toggle => {
-    toggle.addEventListener("click", (e) => {
-      e.preventDefault();
-      const dropdownMenu = toggle.nextElementSibling;
-
-      if (dropdownMenu.style.display === "block") {
-        dropdownMenu.style.display = "none";
-        toggle.querySelector(".dropdown-icon").textContent = "+";
-      } else {
-        dropdownMenu.style.display = "block";
-        toggle.querySelector(".dropdown-icon").textContent = "–";
-    }
-  });
-});
-
