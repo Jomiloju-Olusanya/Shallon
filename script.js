a72519a02ad5a0cc46205a6bd65de075a5ef4da3
Jyay <anjiolusanya@gmail.com>
2025-04-17 13:13:10 +0100

    Update
    CSS & JS

--- script.js
+++ script.js
@@ -4,11 +4,3 @@ const navbar = document.getElementById('navbar');
   menuToggle.addEventListener('click', () => {
     navbar.classList.toggle('show');
 });
-
-// Mobile menu toggle
-const menuToggle = document.getElementById("menu-toggle");
-const navbar = document.getElementById("navbar");
-
-  menuToggle.addEventListener("click", () => {
-    navbar.classList.toggle("show");
-});
