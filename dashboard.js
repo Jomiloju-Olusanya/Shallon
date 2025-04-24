a4246e5bb8396830319158dd9ff6663f3c54255a
Jyay <anjiolusanya@gmail.com>
2025-04-24 14:23:06 +0100

    dashboard.html .css .js
    Set eye-toggle to default hidden

--- dashboard.js
+++ dashboard.js
@@ -15,3 +15,21 @@ toggleBtn.addEventListener('click', () => {
     ? '<i class="fas fa-eye-slash"></i>' 
     : '<i class="fas fa-eye"></i>';
 });
+
+const toggleBtn = document.getElementById('toggleBalance');
+const balance = document.getElementById('walletBalance');
+
+let isVisible = false;
+
+toggleBtn.addEventListener('click', () => {
+  if (isVisible) {
+    balance.innerHTML = '&#8358;****';
+    balance.classList.remove('visible-balance');
+    balance.classList.add('hidden-balance');
+  } else {
+    balance.innerHTML = '&#8358;10,500';
+    balance.classList.remove('hidden-balance');
+    balance.classList.add('visible-balance');
+  }
+  isVisible = !isVisible;
+});
