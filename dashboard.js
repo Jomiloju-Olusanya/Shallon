60ccaffba42ef70837ef031f275556f84cb22383
Jyay <anjiolusanya@gmail.com>
2025-04-24 14:42:58 +0100

    wallet balance

--- dashboard.js
+++ dashboard.js
@@ -15,24 +15,3 @@ toggleBtn.addEventListener('click', () => {
     ? '<i class="fas fa-eye-slash"></i>' 
     : '<i class="fas fa-eye"></i>';
 });
-
-const toggleBtn = document.getElementById('toggleBalance');
-const walletBalance = document.getElementById('walletBalance');
-  let isVisible = false;
-  const actualBalance = '&#8358;10,500';
-
-  toggleBtn.addEventListener('click', () => {
-    isVisible = !isVisible;
-    
-    if (isVisible) {
-      walletBalance.innerHTML = actualBalance;
-      walletBalance.classList.remove('hidden-balance');
-      walletBalance.classList.add('visible-balance');
-      toggleBtn.textContent = '👁️'; // eye open
-    } else {
-      walletBalance.innerHTML = '&#8358;****';
-      walletBalance.classList.remove('visible-balance');
-      walletBalance.classList.add('hidden-balance');
-      toggleBtn.textContent = '🙈'; // eye off
-    }
-  });
